b19671f81c8f4ba504d463b46b7a8a341ad736d6
AbhinavGupta-012 <abhinavgupta1702@gmail.com>
2024-04-24 14:36:52 +0530

    Upploaded older version

---
+++ Js/cart.js
@@ -0,0 +1,50 @@
+let cartContent = document.querySelector('.cart-content');
+let cartVisible = true;
+let listCartHTML = document.querySelector('.listCart');
+
+let listProducts = []; 
+
+function showCart(){
+	if (cartVisible){
+		cartContent.style.display = 'none';
+		cartVisible = false;
+	}
+	else{
+		cartContent.style.display = 'block';
+		cartVisible = true;
+	}
+}
+
+const addDataToHTML = () => {
+	listCartHTML.innerHTML = '';
+	if(listProducts.length > 0){
+		listProducts.forEach(product => {
+			let newProduct = document.createElement('div');
+			newProduct.classList.add('item');
+			newProduct.innerHTML = `
+				<img src = "${info.image}">
+				<h2> ${info.name} </h2>
+				<div class = "price"> ${info.price} </div>
+				<button class = "addCart">
+					Add To Cart
+				</button>
+			`;
+			listCartHTML.appendChild(newProduct);
+		})
+	}
+}
+listCartHTML.addEventListener('click', (event) => {
+	let positionClick = event.target;
+	if(positionClick.classList.contains('addCart')){
+		alert('1');
+	}
+})
+
+const initApp = () => {
+	fetch('info.json')
+	.then(response => response.json())
+	.then(data => {
+		listProducts = data;
+	})
+} 
+initApp();
